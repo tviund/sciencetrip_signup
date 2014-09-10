3ce766e1946a80d7e65bc958bd7c50ddba71b4a0
Kristján Oddsson <koddsson@gmail.com>
2014-09-10 22:57:35 +0000

    Delete erroring code

--- server/api/event/event.model.js
+++ server/api/event/event.model.js
@@ -16,15 +16,4 @@ var EventSchema = new Schema({
   groups: [{type: Schema.Types.ObjectId, ref: 'Group'}]
 });
 
-
-
-EventSchema.pre('save', function(next){
-	now = new Date();
-	this.createDate = now;
-	if ( !this.createDate ) {
-		this.createDate = now;
-	}
-	next();
-});
-
 module.exports = mongoose.model('Event', EventSchema);
