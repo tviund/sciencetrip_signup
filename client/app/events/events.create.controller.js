46298a90f91d3a3baff65765d36310a08bd3f564
Kristján Oddsson <koddsson@gmail.com>
2014-10-09 20:07:59 -0700

    Fix up the create events page.

--- client/app/events/events.create.controller.js
+++ client/app/events/events.create.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('tviundApp')
-  .controller('EventsCreateCtrl', function ($scope, $http) {
+  .controller('EventsCreateCtrl', function ($scope, $http, $filter, $location) {
+    
+    $scope.format = 'dd/MM/yyyy';
+    $scope.model = {
+      date: $filter('date')(new Date, 'dd/MM/yyyy'),
+    };
 
-    $scope.model = {};
     $scope.open = function ($event) {
       $event.preventDefault();
       $event.stopPropagation();
@@ -11,28 +15,9 @@ angular.module('tviundApp')
       $scope.opened = true;
     };
 
-    $scope.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1
+    $scope.submitEvent = function ($event) {
+      $http.post('/api/events', $scope.model).success(function(data) {
+        $location.path('/events').replace();
+      });
     };
-
-    $scope.initDate = new Date();
-    $scope.format = 'dd/MM/yyyy';
-
-    $scope.$watch('time', function (val) {
-      console.log(val);
-    });
-
-    $scope.createEvent = function (form) {
-      if(!$scope.time){
-        $scope.time = new Date();
-      }
-      form.startEventDate = new Date($scope.dt);
-      form.startEventDate.setHours($scope.time.getHours(), $scope.time.getMinutes(), $scope.time.getSeconds());
-      console.log(form);
-      $http.post('/api/events',form).success(function(){
-        alert();
-      })
-    }
-
   });
