2f6348db85560b993c91ac77c6bb137126f335dd
Kristján Oddsson <koddsson@gmail.com>
2014-10-13 18:37:37 -0700

    Changes after comments on PR.

--- client/app/app.js
+++ client/app/app.js
@@ -36,13 +36,9 @@
 		};
 	}
 
-	function appRun($rootScope, $location, Auth, $state) {
+	function appRun($rootScope, $location, Auth) {
 		// Redirect to login if route requires auth and you're not logged in
-		$rootScope.$on('$stateChangeStart', function(event, toState, next) {
-            if (toState.requireLogin) { 
-                $state.transitionTo("login");
-                event.preventDefault();
-            }
+		$rootScope.$on('$stateChangeStart', function (event, next) {
 			Auth.isLoggedInAsync(function (loggedIn) {
 				if (next.authenticate && !loggedIn) {
 					$location.path('/login');
--- client/app/events/events.js
+++ client/app/events/events.js
@@ -1,35 +1,36 @@
 'use strict';
 
 (function () {
-	function eventsConfig($stateProvider) {
-		$stateProvider
-			.state('events', {
-				url: '/events',
-				templateUrl: 'app/events/events.html',
-				controller: 'EventsCtrl',
-				controllerAs: 'vm'
-			})
-			.state('create', {
-				url: '/events/create',
-				templateUrl: 'app/events/events.create.html',
-				controller: 'EventsCreateCtrl',
-				controllerAs: 'vm',
-                               requireLogin: true
+    function eventsConfig($stateProvider) {
+        $stateProvider
+            .state('events', {
+                url: '/events',
+                templateUrl: 'app/events/events.html',
+                controller: 'EventsCtrl',
+                controllerAs: 'vm'
+            })
+            .state('create', {
+                url: '/events/create',
+                templateUrl: 'app/events/events.create.html',
+                controller: 'EventsCreateCtrl',
+                controllerAs: 'vm',
+                authenticate: true
 
-			})
-			.state('edit', {
-				url: '/events/edit/:id',
-				templateUrl: 'app/events/events.edit.html',
-				controller: 'EventsEditCtrl'
-			})
-			.state('events-details', {
-				url: '/events/:id',
-				templateUrl: 'app/events/events.details.html',
-				controller: 'EventsDetailsCtrl',
-				controllerAs: 'vm'
-			})
-	};
+            })
+            .state('edit', {
+                url: '/events/edit/:id',
+                templateUrl: 'app/events/events.edit.html',
+                controller: 'EventsEditCtrl',
+                authenticate: true
+            })
+            .state('events-details', {
+                url: '/events/:id',
+                templateUrl: 'app/events/events.details.html',
+                controller: 'EventsDetailsCtrl',
+                controllerAs: 'vm'
+            })
+    };
 
-	angular.module('tviundApp')
-		.config(eventsConfig);
+    angular.module('tviundApp')
+        .config(eventsConfig);
 })();
