da3e2639031983e44cffcb9039bac7810f7c2090
Kristján Oddsson <koddsson@gmail.com>
2014-10-13 21:02:50 -0700

    Add the ability to delete students. Closes #9.

--- client/app/admin/admin.user.list.controller.js
+++ client/app/admin/admin.user.list.controller.js
@@ -1,11 +1,19 @@
 (function () {
-	'use strict';
-	function adminUserListCtrl($scope, $http) {
-		$http.get('/api/users').success(function (users) {
-			$scope.users = users;
-		});
-	}
+    'use strict';
+    function adminUserListCtrl($scope, $http, $log) {
+        $http.get('/api/users').success(function (users) {
+            $scope.users = users;
+        });
+        
+        $scope.delete = function(user_id) {
+            $http.delete('/api/users/' + user_id).success(function() {
+                $log.log($scope.users);
+                $scope.users = _.reject($scope.users, function(user) { return user._id === user_id; }); 
+                $log.log($scope.users);
+            });
+        };
+    };
 
-	angular.module('tviundApp')
-		.controller('AdminUserListCtrl', adminUserListCtrl);
+    angular.module('tviundApp')
+        .controller('AdminUserListCtrl', adminUserListCtrl);
 })();
